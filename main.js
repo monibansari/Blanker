83ac4ddb205042a2b8c340b690c5fa98f1a72307
monibansari <monibansari18@gmail.com>
2025-04-26 17:20:11 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
 async function safetyChecker(x) {
     const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-
     try {
         await delay(2000);
-
         let url = x;
         let security_key = "True";
-
         const response = await fetch(`https://raw.githubusercontent.com/monibansari/blanker/main/clients/${url}.txt`);
-
         let customer_security_key = await response.text();
         customer_security_key = customer_security_key.trim()
-
         if (security_key === customer_security_key) {
             console.log("YES");
             // console.log(customer_security_key,security_key)
